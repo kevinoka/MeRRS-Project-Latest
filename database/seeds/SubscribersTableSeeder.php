ddf42c5874a28db35c6de082bac39301d694cd2c
kevinoka <kevinoka696@gmail.com>
2020-02-17 11:10:41 +0700

    Add subscribers db seeder

---
+++ database/seeds/SubscribersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SubscribersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('subscribers')->insert([
+            'email' => 'aswinbuana3@gmail.com',
+        ]);
+    }
+}
